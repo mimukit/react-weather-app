4911fcdc702789f76c9f427df44b2e2649177de1
MI MUKIT <imukit007@gmail.com>
2017-06-10 15:35:02 +0600

    Update WeatherDisplay component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,63 @@ import './App.css';
 
 
 class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({ weatherData: json });
+    });
+  }
   render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
     return (
-      <h1>Zip: {this.props.zip}</h1>
+      <div>
+        <h1>
+          {weather.main} in {weatherData.name}
+          <img src={iconUrl} alt={weatherData.description} />
+        </h1>
+        <p>Current: {weatherData.main.temp}°</p>
+        <p>High: {weatherData.main.temp_max}°</p>
+        <p>Low: {weatherData.main.temp_min}°</p>
+        <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+      </div>
     );
   }
 }
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0
+    }
+  }
   render() {
     return (
       <div className="App">
-        <WeatherDisplay zip={"12345"} />
+        <WeatherDisplay zip={PLACES[this.state.activePlace].zip} key={this.state.activePlace}/>
+        {PLACES.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index});
+            }}
+          >
+              {place.name}
+          </button>
+        ))}
       </div>
     );
   }
