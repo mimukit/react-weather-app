492b32395e09be541a8f512c72b9e8a9d9db340d
MI MUKIT <imukit007@gmail.com>
2017-06-10 15:40:52 +0600

    Add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PLACES from './data'
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
 import './App.css';
+
 
 
 
